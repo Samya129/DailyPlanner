995b57ff9693a5e0d5a59e90c61db6fd0cd66127
Sam129 <Samyaf020@gmail.com>
2020-12-05 17:30:33 -0500

    Figured out date on the top of the planner. Yay! Re-formatted time to look identical.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,12 @@
 var timeBlock = $(".time-block");
 
-//for timeBlock( var i= 0; i<timeBlock; i++){
+//for timeBlock ( var i= 0; i< timeBlock; i++){
    
 
 
-    //var now = DateTime.local();
-    
+//get element by id for jQuery
+//var dt = DateTime.local( month, day, year);
+//$("#currentDay").text(dt);
+
+//year.month.day.hour.minute.
+$("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
