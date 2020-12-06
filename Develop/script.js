faa377bd912c27b1e3128cf15d4316bbdbe908ef
Sam129 <Samyaf020@gmail.com>
2020-12-06 14:52:31 -0500

    Got current time to appear in console.

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,41 @@
-var timeBlock = $(".time-block");
 
-//for timeBlock ( var i= 0; i< timeBlock; i++){
+$("#currentDay").text(luxon.DateTime.local().toLocaleString({
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+})
+);   
+
+
+//time block for present, past and future. 
+let currentTime = luxon.DateTime.local().toLocaleString({
+
+hour: "2-digit",
+});
+
+console.log(currentTime);
+        
+function PPF(){
+
+
+    
+}
    
+
 
 
 //get element by id for jQuery
 //var dt = DateTime.local( month, day, year);
 //$("#currentDay").text(dt);
-
-//year.month.day.hour.minute.
-$("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
-
+var timeBlock = $(".time-block");
 var hour = $(".hour");
 var timeBlock = $(".time-block");
 var saveBtn = $(".saveBtn");
-var 
 
-var agenda = [];
 
-init()
+//var agenda = [];
+
+//init()
 
 function init(){
     // Get stored agenda from localStorage
@@ -30,6 +47,8 @@ function init(){
     agenda = storedAgenda;
   }
 
-  // Render todos to the DOM
-  renderAgenda();
+  // Render agenda to the DOM
+  //renderAgenda();
 }
+//for timeBlock ( var i= 0; i< timeBlock; i++){
+    //year.month.day.hour.minute.
