2b9e6a799aac4cdc4d7cd0cf8bdcd40fa4b5df14
Sam129 <Samyaf020@gmail.com>
2020-12-06 13:50:15 -0500

    Added possible variables to use for storage.

--- Develop/script.js
+++ Develop/script.js
@@ -10,3 +10,26 @@ var timeBlock = $(".time-block");
 
 //year.month.day.hour.minute.
 $("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
+
+var hour = $(".hour");
+var timeBlock = $(".time-block");
+var saveBtn = $(".saveBtn");
+var 
+
+var agenda = [];
+
+init()
+
+function init(){
+    // Get stored agenda from localStorage
+  // Parsing the JSON string to an object
+  var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
+
+  // If agenda were retrieved from localStorage, update the agenda array to it
+  if (storedAgenda !== null) {
+    agenda = storedAgenda;
+  }
+
+  // Render todos to the DOM
+  renderAgenda();
+}
